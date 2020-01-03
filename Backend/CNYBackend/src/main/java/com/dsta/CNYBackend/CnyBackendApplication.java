3bba7214d2df22f4d906748c120250e797850e4e
lminglia <lminglia@cn.eit>
2020-01-03 18:43:10 +0800

    added created at for user entity

--- Backend/CNYBackend/src/main/java/com/dsta/CNYBackend/CnyBackendApplication.java
+++ Backend/CNYBackend/src/main/java/com/dsta/CNYBackend/CnyBackendApplication.java
@@ -2,8 +2,10 @@ package com.dsta.CNYBackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class CnyBackendApplication {
 
 	public static void main(String[] args) {
--- Backend/CNYBackend/src/main/java/com/dsta/CNYBackend/User/User.java
+++ Backend/CNYBackend/src/main/java/com/dsta/CNYBackend/User/User.java
@@ -1,14 +1,21 @@
 package com.dsta.CNYBackend.User;
 
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import java.time.LocalDateTime;
+
 
 @Entity
 @Table(name = "USER")
+@EntityListeners(AuditingEntityListener.class)
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -16,6 +23,10 @@ public class User {
 
     @Column(name = "USERNAME", length = 50, unique = true)
     private String username;
+
+    @Column(name="CREATED_AT")
+    @CreatedDate
+    private LocalDateTime createAt;
 
     public Long getId() {
         return id;
