d63ae7aa80d1e2b96e4567f7538196c92e64917c
lminglia <lminglia@cn.eit>
2020-01-06 17:28:39 +0800

    supress cors for websocket

--- src/main/java/com/dsta/CNYBackend/Game/WebSocketConfig.java
+++ src/main/java/com/dsta/CNYBackend/Game/WebSocketConfig.java
@@ -18,7 +18,10 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/game").setAllowedOrigins("http://localhost:4200", "http://localhost:8080", "http://localhost:3000").withSockJS();
+        registry.addEndpoint("/game")
+                .setAllowedOrigins("http://localhost:4200", "http://localhost:8080", "http://localhost:3000")
+                .withSockJS()
+                .setSupressCors(true);
     }
 
 }
