258a85d8c1a0fd845cf39de4c7ce6e43d0211e9d
lminglia <lminglia@cn.eit>
2020-01-06 13:24:08 +0800

    add config for web socket

--- src/main/java/com/dsta/CNYBackend/shared/security/WebSecurityConfig.java
+++ src/main/java/com/dsta/CNYBackend/shared/security/WebSecurityConfig.java
@@ -50,7 +50,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
                 .csrf().disable()
-                .authorizeRequests().antMatchers("/authenticate", "/api/user/create").permitAll()
+                .authorizeRequests().antMatchers("/authenticate", "/api/user/create", "/topic/*", "/game").permitAll()
                 .and()
                 .authorizeRequests()
                 .antMatchers("/",
