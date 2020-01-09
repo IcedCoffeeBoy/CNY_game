e1e0fddb6e261e9e83388fa1c37907afb59394e9
lminglia <lminglia@cn.eit>
2020-01-09 15:50:56 +0800

    added HTTPS for port 8443

--- src/main/java/com/dsta/CNYBackend/CnyBackendApplication.java
+++ src/main/java/com/dsta/CNYBackend/CnyBackendApplication.java
@@ -1,7 +1,12 @@
 package com.dsta.CNYBackend;
 
+
+import org.apache.catalina.connector.Connector;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import springfox.documentation.builders.PathSelectors;
@@ -27,5 +32,15 @@ public class CnyBackendApplication {
                 .paths(PathSelectors.any())
                 .build()
                 .pathMapping("/");
+    }
+
+    @Bean
+    public ServletWebServerFactory servletContainer(@Value("${server.http.port}") int httpPort) {
+         Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+        connector.setPort(httpPort);
+
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+        tomcat.addAdditionalTomcatConnectors(connector);
+        return tomcat;
     }
 }
