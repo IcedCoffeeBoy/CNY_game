90edaf4d3ce5f5446a2c5632cc624a7ec847faaa
lminglia <lminglia@cn.eit>
2020-01-09 17:09:46 +0800

    changed submit answer to post'

--- src/main/java/com/dsta/CNYBackend/answer/AnswerController.java
+++ src/main/java/com/dsta/CNYBackend/answer/AnswerController.java
@@ -10,6 +10,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -31,7 +32,7 @@ public class AnswerController {
     }
 
     @ApiOperation(value = "Submit choice for question(position). Requires JWT token header")
-    @RequestMapping(value = "/submit/{position}", method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(value = "/submit/{position}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Map<String, String>> submitAnswer(
             @PathVariable("position") Integer position,
             @RequestBody Choice choice,
