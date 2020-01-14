f23221142bbe619808289a4447db6e7b34a9c897
lminglia <lminglia@cn.eit>
2020-01-14 13:15:17 +0800

    Added more info in JWT warning log

--- src/main/java/com/dsta/CNYBackend/shared/security/JwtRequestFilter.java
+++ src/main/java/com/dsta/CNYBackend/shared/security/JwtRequestFilter.java
@@ -32,7 +32,7 @@ public class JwtRequestFilter extends OncePerRequestFilter {
         String jwtToken = null;
         // JWT Token is in the form "Bearer token". Remove Bearer word and get
         // only the Token
-        
+
         if (requestTokenHeader != null && requestTokenHeader.startsWith("Bearer")) {
             jwtToken = requestTokenHeader.substring(7);
             try {
@@ -43,7 +43,7 @@ public class JwtRequestFilter extends OncePerRequestFilter {
                 System.out.println("JWT Token has expired");
             }
         } else {
-            logger.warn("JWT Token does not begin with Bearer String");
+            logger.warn("JWT Token does not begin with Bearer String \nfor: " + request.getRequestURI() + " , \nfrom: " + request.getRemoteUser());
         }
         // Once we get the token validate it.
         if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
