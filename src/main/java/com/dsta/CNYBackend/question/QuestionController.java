cbd8f9a7ee9878fc47ff3bd4a555094cd89d68dd
lminglia <lminglia@cn.eit>
2020-01-15 13:03:16 +0800

    Revert "Added reactive rest for question"
    This reverts commit 15785969d06cbb213f4182c9cd4b7495d9eb5ab2.

--- src/main/java/com/dsta/CNYBackend/question/QuestionController.java
+++ src/main/java/com/dsta/CNYBackend/question/QuestionController.java
@@ -7,8 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import rx.Single;
-import rx.schedulers.Schedulers;
 
 import java.util.List;
 
@@ -29,19 +27,15 @@ public class QuestionController {
 //    }
 
     @GetMapping(value = "/{position}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public Single<ResponseEntity<Question>> getQuestion(@PathVariable("position") Integer position) {
-        return this.questionService.getQuestionByPositionObservable(position)
-                .subscribeOn(Schedulers.io())
-                .map(question -> ResponseEntity.ok(question))
-                .onErrorReturn(error -> ResponseEntity.notFound().build());
+    public ResponseEntity<Question> getQuestion(@PathVariable("position") Integer position) {
+        Question question = this.questionService.getQuestionByPosition(position);
+        return ResponseEntity.ok(question);
     }
 
     @GetMapping(value = "/all", produces = MediaType.APPLICATION_JSON_VALUE)
-    public Single<ResponseEntity<List<Question>>> getAllQuestions() {
-        return this.questionService.getAllQuestionsObservable()
-                .subscribeOn(Schedulers.io())
-                .map(questions -> ResponseEntity.ok(questions))
-                .onErrorReturn(error -> ResponseEntity.notFound().build());
+    public ResponseEntity<List<Question>> getAllQuestions() {
+        List<Question> questions = this.questionService.getAllQuestions();
+        return ResponseEntity.ok(questions);
     }
 
     @GetMapping(value = "/all-normal", produces = MediaType.APPLICATION_JSON_VALUE)
