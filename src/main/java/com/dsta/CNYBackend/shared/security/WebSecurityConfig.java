2596a348a6d56a7426394e6d3e5de91084101131
Lee Ming Liang <lee.mingliang@outlook.com>
2020-01-19 15:15:02 +0800

    Added react web app

--- src/main/java/com/dsta/CNYBackend/shared/security/WebSecurityConfig.java
+++ src/main/java/com/dsta/CNYBackend/shared/security/WebSecurityConfig.java
@@ -37,7 +37,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
             "/api/user/create", "/api/question/*", "/api/poll/*", "/api/game/state", "/api/game/participants", "/api/game/rank"
     };
     public static final String[] ALLOWED_SWAGGER_RESOURCES = {"/swagger-resources/**", "/swagger-ui.html", "/v2/api-docs", "/webjars/**"};
-    public static final String[] ALLOWED_RESOURCES = {"/", "/favicon.ico", "/**/*.png", "/**/*.gif", "/**/*.svg", "/**/*.jpg", "/**/*.html", "/**/*.css", "/**/*.js","/**/*.json*", "/public/*"};
+    public static final String[] ALLOWED_RESOURCES = {"/", "/favicon.ico", "/**/*.png", "/**/*.gif", "/**/*.svg", "/**/*.jpg", "/**/*.html", "/**/*.css", "/**/*.js", "/**/*.json*", "/static/**"};
     public static final String[] ALL_API = {"/api/question/*", "/api/game/*", "/api/user/*", "/api/poll/*", "/authenticate", "/topic/*", "/game/*", "/actuator"};
 
     @Value("${jwt.protect:false}")
@@ -80,19 +80,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
             }
 
             @Override
-            public void addViewControllers(ViewControllerRegistry registry) {
-                registry.addRedirectViewController("/", "index.html");
-            }
-
-
-            @Override
             public void addResourceHandlers(ResourceHandlerRegistry registry) {
                 registry.addResourceHandler("swagger-ui.html")
                         .addResourceLocations("classpath:/META-INF/resources/");
 
                 registry.addResourceHandler("/webjars/**")
                         .addResourceLocations("classpath:/META-INF/resources/webjars/");
-
 
                 registry.addResourceHandler("/static/**")
                         .addResourceLocations("classpath:/public/static/");
